73c3e991ad520f2b41e539ace61c231d2b30bb1d
Eetay Natan <eetay.natan@samanage.com>
2018-10-11 13:32:02 +0300

    wip

--- SL1/Connection.js
+++ SL1/Connection.js
@@ -11,23 +11,49 @@ let Connection = function(params) {
 Connection.prototype = {
   send: function(request) {
     let headers = new fetch.Headers()
-    let { connection, path } = request
+    let { connection, path, method, payload } = request
     let { username, password, origin } = connection.params
     let url = [origin, path].join('/')
-    console.log(url)
     headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'))
     headers.append('Accept', 'application/json')
-    return fetch(url, {
-      agent,
-      method: 'GET',
-      headers
-    }).then( response => {
+    headers.set('Content-Type', 'application/json')
+    options = { agent, method, headers }
+    if (payload) options.body = JSON.stringify(payload)
+    console.log(options)
+    return fetch(url, options).then( response => {
       //response.text().then(text => console.log('xxx', text))
-      return response.json()
+      return (request.parser == 'text') ? response.text() : response.json()
     })
+  },
+  describeAPIRequest() {
+    var request = {
+      method: 'GET',
+      connection: this,
+      path: '/opt/iservices/scripts/swagger.yml',
+      parser: 'text'
+    }
+    request.send = function() {
+      return request.connection.send(request)
+    }
+    return request
+  },
+  uploadIntegrationApplicationRequest(appManifestPath) {
+    var request = {
+      method: 'POST',
+      connection: this,
+      path: `/api/v1/applications`,
+      payload: require(['..', appManifestPath].join('/')),
+      parser: 'text'
+    }
+    console.log(request)
+    request.send = function() {
+      return request.connection.send(request)
+    }
+    return request
   },
   getIntegrationApplicationRequest(appName) {
     var request = {
+      method: 'GET',
       connection: this,
       path: `/api/v1/applications/${appName}`
     }
@@ -38,6 +64,7 @@ Connection.prototype = {
   },
   getStepRequest: function(stepName) {
     var request = {
+      method: 'GET',
       connection: this,
       path: `/api/v1/steps/${stepName}`
     }
--- index.js
+++ index.js
@@ -6,6 +6,17 @@ let sl = new SL1.Connection(require('./config.json'))
 //   json => console.log(json.data)
 // )
 
-sl.getIntegrationApplicationRequest('integration_template').send().then(
+// sl.getIntegrationApplicationRequest('integration_template').send().then(
+//   json => console.log(JSON.stringify(json))
+// )
+
+sl.uploadIntegrationApplicationRequest('./samanage-sync/samanage-sync.json').send().then(
   json => console.log(JSON.stringify(json))
 )
+
+// sl.describeAPIRequest().send().then(
+//   text => console.log(text.replace(/=\/static/g, `=https://18.212.47.252/static`))
+// )
+
+
+
